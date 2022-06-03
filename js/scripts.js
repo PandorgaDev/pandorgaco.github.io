a7257087e836302c73d137dbfb1479cee8d2d548
Felix Ayala <felix1262@gmail.com>
2022-06-03 19:46:13 -0400

    Save legacy version

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+(function($){
+	
+	$(window).on('load', function(){
+		$('.fade-in').css({ position: 'relative', opacity: 0, top: -14 });
+		setTimeout(function(){
+			$('#preload-content').fadeOut(400, function(){
+				$('#preload').fadeOut(800);
+				setTimeout(function(){
+					$('.fade-in').each(function(index) {
+						$(this).delay(400*index).animate({ top : 0, opacity: 1 }, 800);
+					});
+				}, 800);
+			});
+		}, 400);
+	});
+
+	$(document).ready( function(){
+
+		// Create a countdown instance. Change the launchDay according to your needs.
+		// The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
+		// Thus the launchDay below denotes 7 May, 2014.
+		// var launchDay = new Date(2014, 9-1, 7);
+		// $('#timer').countdown({
+		// 	until: launchDay
+		// });
+
+		// Add background image
+		$.backstretch('images/bg1.jpg');
+
+		// Invoke the Placeholder plugin
+		$('input, textarea').placeholder();
+
+		// Open modal window on click
+		$('#modal-open').on('click', function(e) {
+			var mainInner = $('#main .inner'),
+				modal = $('#modal');
+
+			mainInner.animate({ opacity: 0 }, 400, function(){
+				$('html,body').scrollTop(0);
+				modal.addClass('modal-active').fadeIn(400);
+			});
+			e.preventDefault();
+
+			$('#modal-close').on('click', function(e) {
+				modal.removeClass('modal-active').fadeOut(400, function(){
+					mainInner.animate({ opacity: 1 }, 400);
+				});
+				e.preventDefault();
+			});
+		});
+
+	});
+	
+})(jQuery);
